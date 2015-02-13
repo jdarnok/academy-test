696c7f63be4509e115acfa5ae5572bbc857eef9f
Konrad Janczyk <janczyk.konrad@gmail.com>
2015-02-13 13:50:13 +0100

    stage 4: added city_id to users, added city fields in users devise form, added city_id to the registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :age, :name)
+    params.require(:user).permit(:email, :password, :password_confirmation, :age, :name, :city_id)
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :age, :name, :current_password)
+    params.require(:user).permit(:email, :password, :password_confirmation, :age, :name, :current_password, :city_id)
   end
 end
